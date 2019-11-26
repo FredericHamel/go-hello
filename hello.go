42db69a67cca2f1e97d4f70e70e931a8aac98c6c
Frederic Hamel <frederic.hamel123@gmail.com>
2019-11-25 21:19:33 -0500

    first commit

---
+++ hello.go
@@ -0,0 +1,7 @@
+package hello
+import "fmt"
+
+func Hi(name string) {
+  fmt.Printf("hello %s\n", name)
+}
+
---
+++ hello_test.go
@@ -0,0 +1,41 @@
+package hello
+import (
+  "bytes"
+  "io"
+  "os"
+  "testing"
+)
+
+func TestHi(t *testing.T) {
+  stdout := os.Stdout
+
+  r, w, err := os.Pipe()
+  if err != nil {
+    t.Error("Unable to create Pipe")
+    return
+  }
+  os.Stdout = w
+
+  defer func () {
+    os.Stdout = stdout
+  }()
+
+  resultC := make(chan string)
+  go func() {
+    defer w.Close()
+    Hi("Max")
+  }()
+
+  go func() {
+    var buf bytes.Buffer
+    io.Copy(&buf, r)
+    resultC <- buf.String()
+  }()
+
+  expected := "hello Max\n"
+
+  result := <-resultC
+  if result != expected {
+    t.Errorf("hello.Hi(\"Max\") => %s; expected \"hello Max\"", result)
+  }
+}
